161bac669173960b82faf279f9ef9399f8fa9087
Karina Miernik <karina.miernik@gmail.com>
2020-06-06 12:42:54 +0200

    smooth scroll added

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+// -----------Hamburger Menu -----------//
+
 const hamburgerOpen = document.querySelector('.hamburger')
 const hamburgerElement = document.querySelector('.hamburger__element')
 const menu = document.querySelector('.menu')
@@ -18,3 +21,57 @@ for (let i = 0; i < menuLink.length; i++) {
         menu.classList.remove('open')
     });
 }
+
+// -----------Smooth scroll -----------//
+
+const anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+anchorLinks.forEach((link) => {
+    let scrollTarget = link.getAttribute('href')
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        scrollTo(scrollTarget, 500)
+    })
+})
+
+function scrollTo(to, duration) {
+    //t = current time
+    //b = start value
+    //c = change in value
+    //d = duration
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t * t + b;
+        t -= 2;
+        return c / 2 * (t * t * t + 2) + b;
+    };
+
+    return new Promise((resolve, reject) => {
+        const element = document.scrollingElement;
+
+        if (typeof to === 'string') {
+            to = document.querySelector(to) || reject();
+        }
+        if (typeof to !== 'number') {
+            to = to.getBoundingClientRect().top + element.scrollTop;
+        }
+
+        let start = element.scrollTop,
+            change = to - start,
+            currentTime = 0,
+            increment = 20;
+
+        const animateScroll = function () {
+            currentTime += increment;
+            let val = ease(currentTime, start, change, duration);
+            element.scrollTop = val;
+            if (currentTime < duration) {
+                setTimeout(animateScroll, increment);
+            } else {
+                resolve();
+            }
+        };
+        animateScroll();
+    });
+}
+
