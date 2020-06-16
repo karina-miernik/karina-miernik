e621b25ff5832de01f07cc8f7ee8d844ac21e83d
Karina Miernik <karina.miernik@gmail.com>
2020-06-16 22:06:49 +0200

    arrow go up added

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // -----------Hamburger Menu -----------//
 
 const hamburgerOpen = document.querySelector('.hamburger')
@@ -8,102 +7,122 @@ const menuLink = document.querySelectorAll('.menu__link')
 const headerLogo = document.querySelector('.header__logo')
 
 hamburgerOpen.addEventListener('click', () => {
-    menu.classList.toggle('open')
-    hamburgerOpen.classList.toggle('change')
-
-
+  menu.classList.toggle('open')
+  hamburgerOpen.classList.toggle('change')
 })
 
 for (let i = 0; i < menuLink.length; i++) {
-    menuLink[i].addEventListener("click", () => {
-        menu.classList.remove('open')
-        hamburgerOpen.classList.remove('change')
-    });
+  menuLink[i].addEventListener('click', () => {
+    menu.classList.remove('open')
+    hamburgerOpen.classList.remove('change')
+  })
 }
 
 // -----------Smooth scroll -----------//
 
-const anchorLinks = document.querySelectorAll('a[href^="#"]');
+const anchorLinks = document.querySelectorAll('a[href^="#"]')
 
 anchorLinks.forEach((link) => {
-    let scrollTarget = link.getAttribute('href')
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        scrollTo(scrollTarget, 500)
-    })
+  let scrollTarget = link.getAttribute('href')
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    scrollTo(scrollTarget, 500)
+  })
 })
 
 function scrollTo(to, duration) {
-    //t = current time
-    //b = start value
-    //c = change in value
-    //d = duration
-    function ease(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t * t + b;
-        t -= 2;
-        return c / 2 * (t * t * t + 2) + b;
-    };
+  //t = current time
+  //b = start value
+  //c = change in value
+  //d = duration
+  function ease(t, b, c, d) {
+    t /= d / 2
+    if (t < 1) return (c / 2) * t * t * t + b
+    t -= 2
+    return (c / 2) * (t * t * t + 2) + b
+  }
 
-    return new Promise((resolve, reject) => {
-        const element = document.scrollingElement;
+  return new Promise((resolve, reject) => {
+    const element = document.scrollingElement
 
-        if (typeof to === 'string') {
-            to = document.querySelector(to) || reject();
-        }
-        if (typeof to !== 'number') {
-            to = to.getBoundingClientRect().top + element.scrollTop;
-        }
+    if (typeof to === 'string') {
+      to = document.querySelector(to) || reject()
+    }
+    if (typeof to !== 'number') {
+      to = to.getBoundingClientRect().top + element.scrollTop
+    }
 
-        let start = element.scrollTop,
-            change = to - start,
-            currentTime = 0,
-            increment = 20;
+    let start = element.scrollTop,
+      change = to - start,
+      currentTime = 0,
+      increment = 20
 
-        const animateScroll = function () {
-            currentTime += increment;
-            let val = ease(currentTime, start, change, duration);
-            element.scrollTop = val;
-            if (currentTime < duration) {
-                setTimeout(animateScroll, increment);
-            } else {
-                resolve();
-            }
-        };
-        animateScroll();
-    });
+    const animateScroll = function () {
+      currentTime += increment
+      let val = ease(currentTime, start, change, duration)
+      element.scrollTop = val
+      if (currentTime < duration) {
+        setTimeout(animateScroll, increment)
+      } else {
+        resolve()
+      }
+    }
+    animateScroll()
+  })
 }
 
 // ----------- Title animation -----------//
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
     function animateSgv(id, delay, delayIncrement) {
-        const logo = document.getElementById(id);
-        const logoPaths = document.querySelectorAll(`#${id} path`);
-        delay = delay;
-        for (let i = 0; i < logoPaths.length; i++) {
-            //console.log(logoPaths[i].getTotalLength());
-            logoPaths[i].style.strokeDasharray = logoPaths[i].getTotalLength();
-            logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength();
-            logoPaths[i].style.animation = `line-anim 2s ease forwards ${delay}s`;
-            delay += delayIncrement;
-            console.log(delay)
-        }
-        logo.style.animation = `fill 0.5s ease forwards ${delay}s`;
+      const logo = document.getElementById(id)
+      const logoPaths = document.querySelectorAll(`#${id} path`)
+      delay = delay
+      for (let i = 0; i < logoPaths.length; i++) {
+        //console.log(logoPaths[i].getTotalLength());
+        logoPaths[i].style.strokeDasharray = logoPaths[i].getTotalLength()
+        logoPaths[i].style.strokeDashoffset = logoPaths[i].getTotalLength()
+        logoPaths[i].style.animation = `line-anim 2s ease forwards ${delay}s`
+        delay += delayIncrement
+        console.log(delay)
+      }
+      logo.style.animation = `fill 0.5s ease forwards ${delay}s`
     }
     animateSgv('logo', 0.1, 0.1)
-}, false);
+  },
+  false,
+)
 
 // ----------- Navigation - toggle class -----------//
 
-const nav = document.getElementById("nav");
+const nav = document.getElementById('nav')
 window.onscroll = function () {
-    "use strict";
-    if (document.body.scrollTop >= 80 || document.documentElement.scrollTop >= 80) {
-        nav.classList.add("scroll");
-    } else {
-        nav.classList.remove("scroll");
-    }
-};
+  'use strict'
+  if (
+    document.body.scrollTop >= 80 ||
+    document.documentElement.scrollTop >= 80
+  ) {
+    nav.classList.add('scroll')
+  } else {
+    nav.classList.remove('scroll')
+  }
+}
 
+// ----------- Arrow Up -----------//
+const arrow = document.querySelector('.scroll__up')
 
+window.onscroll = function () {
+  'use strict'
+  if (
+    document.body.scrollTop >= 80 ||
+    document.documentElement.scrollTop >= 80
+  ) {
+    arrow.classList.add('scroll__display')
+    arrow.classList.remove('scroll__up')
+  } else {
+    arrow.classList.remove('scroll__display')
+    arrow.classList.add('scroll__up')
+  }
+}
