befd2badf461a790d14b5f1f358ed6dcab47b754
Karina Miernik <karina.miernik@gmail.com>
2020-08-18 15:26:16 +0200

    language added

--- lang.js
+++ lang.js
@@ -9,7 +9,7 @@ let mlCodes = [
 
     {
         code: "pl",
-        name: "Polish"
+        name: "Polski"
     },
 ];
 
@@ -17,152 +17,224 @@ let mlCodes = [
 let MLstrings = [
     {
         English: "Home",
-        Polish: "Strona główna"
+        Polski: "Strona główna"
 
     },
     {
         English: "About",
-        Polish: "O mnie "
+        Polski: "O mnie"
 
     },
     {
         English: "Skills",
-        Polish: "Umiejętności"
+        Polski: "Umiejętności"
+
+    },
+    {
+        English: "Projects",
+        Polski: "Projekty"
 
     },
     {
         English: "Contact",
-        Polish: "Kontakt"
+        Polski: "Kontakt"
 
     },
     {
-        English: "About me.",
-        Polish: "O mnie."
+        English: "ABOUT ME.",
+        Polski: "O MNIE."
 
     },
     {
         English: "By proffesion I am an electrical engineer designing renewables, but I am also at the beginning of my journey with programming.",
-        Polish: "Z zawodu jestem inżynierem elektrotechnikiem, projektującym odnawialne źródła energii, ale jestem również na początku swojej przygody z programowaniem."
+        Polski: "Z zawodu jestem inżynierem elektrotechnikiem, projektującym odnawialne źródła energii, ale jestem również na początku swojej przygody z programowaniem."
 
     },
     {
         English: "I find frontend developing fascinating and that became my biggest passion.",
-        Polish: "Frontend mnie zafascynował i stał się moją najwiekszą pasją."
+        Polski: "Frontend mnie zafascynował i stał się moją najwiekszą pasją."
 
     },
     {
         English: "I'm eager to learn and I'm coding at every possible moment to enhance my skills. I put all my creativity and soul at each project.",
-        Polish: "Jestem chętna do nauki i koduję w każdej możlwiej chwili by polepszyć swoje umiejętności. Wkładam w każdy z projektów całą swoją duszę i kreatywność."
+        Polski: "Jestem chętna do nauki i koduję w każdej możlwiej chwili by polepszyć swoje umiejętności. Wkładam w każdy z projektów całą swoją duszę i kreatywność."
 
     },
     {
-        English: "Skills",
-        Polish: "Umiejętności"
+        English: "SKILLS",
+        Polski: "UMIEJĘTNOŚCI"
 
     },
     {
-        English: "What I'm good at",
-        Polish: "W czym jestem dobra"
+        English: "PROJECTS",
+        Polski: "PROJEKTY"
 
     },
     {
-        English: "What I will be good at",
-        Polish: "W czym będę dobra"
+        English: "ZdrowEat App",
+        Polski: "Aplikacja ZdrowEat"
+
+    },
+    {
+        English: "A team project.",
+        Polski: "Projekt zespołowy."
+
+    },
+    {
+        English: "ZdrowEat is an app for finding heathy recipes. Build in React.",
+        Polski: "ZdrowEat to aplikacja do znajdowania zdrowych przepisów. Zbudowana w Reakcie."
+
+    },
+    {
+        English: "Visit",
+        Polski: "Odwiedź"
+
+    },
+    {
+        English: "BlackJack Game",
+        Polski: "Gra w Oczko"
+
+    },
+    {
+        English: "BlackJack Card Game build in Vanilla JS.",
+        Polski: "Gra karciana Oczko, zbudowana w Vanilla JS."
+
+    },
+    {
+        English: "Visit",
+        Polski: "Odwiedź"
 
     },
     {
         English: "Designing and creating",
-        Polish: "Projektuję i tworzę"
+        Polski: "Projektuję i tworzę"
 
     },
 
     {
         English: "with creativity and passion",
-        Polish: "kreatywnie i z pasją"
+        Polski: "kreatywnie i z pasją"
 
     },
     {
         English: "Location:",
-        Polish: "Lokalizacja:"
+        Polski: "Lokalizacja:"
 
     },
     {
         English: "Gdańsk, Poland",
-        Polish: "Gdańsk, Polska"
+        Polski: "Gdańsk, Polska"
 
     },
     {
         English: "Contact:",
-        Polish: "Kontakt:"
+        Polski: "Kontakt:"
     }
 ];
 
 
 
-let mlrLangInUse;
-let mlr = function (_a) {
-    let _b = _a === void 0 ? {} : _a, _c = _b.dropID, dropID = _c === void 0 ? "mbPOCControlsLangDrop" : _c, _d = _b.stringAttribute, stringAttribute = _d === void 0 ? "data-mlr-text" : _d, _e = _b.chosenLang, chosenLang = _e === void 0 ? "English" : _e, _f = _b.mLstrings, mLstrings = _f === void 0 ? MLstrings : _f, _g = _b.countryCodes, countryCodes = _g === void 0 ? false : _g, _h = _b.countryCodeData, countryCodeData = _h === void 0 ? [] : _h;
-    let root = document.documentElement;
-    let listOfLanguages = Object.keys(mLstrings[0]);
+let mlrLangInUse
+
+let mlr = function ({
+    dropID = "mbPOCControlsLangDrop",
+    stringAttribute = "data-mlr-text",
+    chosenLang = "English",
+    mLstrings = MLstrings,
+    countryCodes = false,
+    countryCodeData = [],
+} = {}) {
+    let root = document.documentElement
+    let listOfLanguages = Object.keys(mLstrings[0])
     mlrLangInUse = chosenLang;
+
     (function createMLDrop() {
-        let mbPOCControlsLangDrop = document.getElementById(dropID);
-        // Reset the menu
-        mbPOCControlsLangDrop.innerHTML = "";
-        // Now build the options
-        listOfLanguages.forEach(function (lang, langidx) {
-            let HTMLoption = document.createElement("option");
-            HTMLoption.value = lang;
-            HTMLoption.textContent = lang;
-            mbPOCControlsLangDrop.appendChild(HTMLoption);
+        let mbPOCControlsLangDrop = document.getElementById(dropID)
+
+        mbPOCControlsLangDrop.innerHTML = ''
+
+        listOfLanguages.forEach(function (lang) {
+            let HTMLoption = document.createElement('option')
+            HTMLoption.value = lang
+            HTMLoption.textContent = lang
+            mbPOCControlsLangDrop.appendChild(HTMLoption)
+
             if (lang === chosenLang) {
-                mbPOCControlsLangDrop.value = lang;
+                mbPOCControlsLangDrop.value = lang
             }
-        });
-        mbPOCControlsLangDrop.addEventListener("change", function (e) {
-            mlrLangInUse = mbPOCControlsLangDrop[mbPOCControlsLangDrop.selectedIndex].value;
-            resolveAllMLStrings();
-            // Here we update the 2-digit lang attribute if required
+        })
+
+        mbPOCControlsLangDrop.addEventListener('change', function (e) {
+            mlrLangInUse =
+                mbPOCControlsLangDrop[mbPOCControlsLangDrop.selectedIndex].value
+            resolveAllMLStrings()
+
             if (countryCodes === true) {
                 if (!Array.isArray(countryCodeData) || !countryCodeData.length) {
-                    console.warn("Cannot access strings for language codes");
-                    return;
+                    console.warn('Cannot access strings for language codes')
+                    return
                 }
-                root.setAttribute("lang", updateCountryCodeOnHTML().code);
+                root.setAttribute('lang', updateCountryCodeOnHTML().code)
             }
-        });
-    })();
+            setLangInStorage()
+        })
+
+        window.onload = function () {
+            if (localStorage.getItem('chosenLang') === 'Polski') {
+
+                mlrLangInUse = localStorage.getItem('chosenLang')
+                mbPOCControlsLangDrop.value = localStorage.getItem('chosenLang')
+                mlr.chosenLang = localStorage.getItem('chosenLang')
+                resolveAllMLStrings()
+
+                if (countryCodes === true) {
+                    if (!Array.isArray(countryCodeData) || !countryCodeData.length) {
+                        console.warn('Cannot access strings for language codes')
+                        return
+                    }
+                    root.setAttribute('lang', mlCodes[0].code)
+                }
+            }
+        }
+    })()
+
+    function setLangInStorage() {
+        localStorage.setItem('chosenLang', mlrLangInUse)
+    }
     function updateCountryCodeOnHTML() {
-        return countryCodeData.find(function (this2Digit) { return this2Digit.name === mlrLangInUse; });
+        return countryCodeData.find(function (this2Digit) {
+            return this2Digit.name === mlrLangInUse
+        })
     }
     function resolveAllMLStrings() {
-        let stringsToBeResolved = document.querySelectorAll("[" + stringAttribute + "]");
+        let stringsToBeResolved = document.querySelectorAll(
+            '[' + stringAttribute + ']',
+        )
         stringsToBeResolved.forEach(function (stringToBeResolved) {
-            let originaltextContent = stringToBeResolved.innerText;
-            let resolvedText = resolveMLString(originaltextContent, mLstrings);
-            stringToBeResolved.innerText = resolvedText;
-        });
+            let originaltextContent = stringToBeResolved.innerText
+            let resolvedText = resolveMLString(originaltextContent, mLstrings)
+            stringToBeResolved.innerText = resolvedText
+        })
     }
-};
+}
 function resolveMLString(stringToBeResolved, mLstrings) {
     let matchingStringIndex = mLstrings.find(function (stringObj) {
-        // Create an array of the objects values:
-        let stringValues = Object.values(stringObj);
-        // Now return if we can find that string anywhere in there
-        return stringValues.includes(stringToBeResolved);
-    });
+        let stringValues = Object.values(stringObj)
+
+        return stringValues.includes(stringToBeResolved)
+    })
     if (matchingStringIndex) {
-        return matchingStringIndex[mlrLangInUse];
-    } //else {
-    //     // If we don't have a match in our language strings, return the original
-    //     return stringToBeResolved;
-    // }
+        return matchingStringIndex[mlrLangInUse]
+    } else {
+        return stringToBeResolved
+    }
 }
 mlr({
-    dropID: "mbPOCControlsLangDrop",
-    stringAttribute: "data-mlr-text",
-    chosenLang: "English",
+    dropID: 'mbPOCControlsLangDrop',
+    stringAttribute: 'data-mlr-text',
+    chosenLang: 'English',
     mLstrings: MLstrings,
     countryCodes: true,
-    countryCodeData: mlCodes
-});
+    countryCodeData: mlCodes,
+})
+
